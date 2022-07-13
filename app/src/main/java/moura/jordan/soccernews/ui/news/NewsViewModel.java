d356b441f647303366bec970cce837f8b6f6ed51
Jordan-moura <jordan.moura16@gmail.com>
2022-07-13 10:38:07 -0300

    Mockando Nossas Noticias Para o RecyclerView

--- app/src/main/java/moura/jordan/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/moura/jordan/soccernews/ui/news/NewsViewModel.java
@@ -4,16 +4,27 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import moura.jordan.soccernews.domain.News;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+         news = new MutableLiveData<>();
+        // TODO Mock De Noticias
+        List<News> temp_news = new ArrayList<>();
+        temp_news.add(new News("Ferroviaria tem Desfalque Importante", "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."));
+        temp_news.add(new News("Ferroviaria joga no Sabado", "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."));
+        temp_news.add(new News("Copa do Mundo feminina esta terminando", "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."));
+
+        news.setValue(temp_news);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
