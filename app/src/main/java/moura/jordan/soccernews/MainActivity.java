b21f636dcafb4fbc56fe2ee69bc91d495ea35f74
Jordan-moura <jordan.moura16@gmail.com>
2022-07-13 15:01:28 -0300

    Finalizando Parte01

--- app/src/main/java/moura/jordan/soccernews/MainActivity.java
+++ app/src/main/java/moura/jordan/soccernews/MainActivity.java
@@ -30,5 +30,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
